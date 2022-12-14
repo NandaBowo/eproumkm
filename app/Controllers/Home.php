65da7390944223dc138731718f74cb3c726e6a02
Galeh-ariya <galehariya8@gmail.com>
2022-12-14 23:32:03 +0700

    menambahkan file sql dan mengubah controler

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $user = new UserModel;
+
+        $us = $user->find();
+        dd($us);
     }
 }
