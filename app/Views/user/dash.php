aabb80b97ce073801f84d98c37e5b99bf6c53a6a
Galeh-ariya <galehariya8@gmail.com>
2022-12-15 10:30:51 +0700

    Fixed dash

--- app/Views/user/dash.php
+++ app/Views/user/dash.php
@@ -1,10 +1,6 @@
 <?php
 $data = session()->get("name");
 
-<<<<<<< HEAD
 // dd(session()->get("loggedIn"));
 
 echo "selamat datang" . $name;
-=======
-echo "selamat datang " . $data;
->>>>>>> 92e41cd3ba53ad16884f871320fbdf87a3ae5200
