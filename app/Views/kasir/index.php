4ab5b8eee970e7ceeefbd22ab94108e5a6ea224b
Galeh-ariya <galehariya8@gmail.com>
2022-12-17 17:21:10 +0700

    ubah dikit view kasir

--- app/Views/kasir/index.php
+++ app/Views/kasir/index.php
@@ -11,22 +11,22 @@
         <div class="p-2 w-100">
             <h1 class="mb-3">Transaksi Penjualan</h1>
 
-            <form>
+            <form action="/kasir/input" method="POST">
                 <div class="mb-3">
                     <label for="tanggal" class="form-label">Tanggal</label>
                     <input type="date" class="form-control" id="tanggal" name="tanggal" aria-describedby="emailHelp" value="<?= $datetime->format("Y-m-d"); ?>" disabled>
                 </div>
                 <div class="mb-3">
                     <label for="namaBarang" class="form-label">Nama Barang</label>
-                    <input type="text" class="form-control" id="namaBarang" aria-describedby="emailHelp">
+                    <input type="text" name="namaBarang" class="form-control" id="namaBarang" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="harga" class="form-label">Harga per Barang</label>
-                    <input type="number" class="form-control" id="harga" aria-describedby="emailHelp">
+                    <input type="number" name="hargaPer" class="form-control" id="harga" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="kuantitas" class="form-label">Kuantitas</label>
-                    <input type="number" class="form-control" id="kuantitas" aria-describedby="emailHelp">
+                    <input type="number" name="quantitas" class="form-control" id="kuantitas" aria-describedby="emailHelp">
                 </div>
                 <button class="btn btn-primary float-end">Submit</button>
             </form>
